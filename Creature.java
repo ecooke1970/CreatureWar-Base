6e4f2f114aa503b2920f1e04c0f428633dd38819
Erik <46272257+ecooke1970@users.noreply.github.com>
2019-11-05 21:43:38 -0500

    Created classes

--- Creature.java
+++ Creature.java
@@ -1,9 +1,10 @@
+import java.util.Random;
 
 /**
  * Write a description of class Creature here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (Erik Cooke) 
+ * @version (2019.11.05)
  */
 public class Creature
 {
@@ -11,12 +12,42 @@ public class Creature
    private int strength;
    
    public Creature(){
-       
+       hp = 10;
+       strength = 10;
    }
-    
-   public int damage(){
-       //TODO: change this
-       return 0;
+   
+   public Creature(int strength, int hp)
+   {
+       if(strength >= 5){
+           this.strength = strength;
+       }
+       else{
+            this.strength = 5;
+       }
+       if(hp >= 5){
+            this.hp = hp;
+       }
+       else{
+            this.hp = 5;
+       }
     }
+    
+   /**
+    * How much damage
+    * @return int - the amount of damage this creature did
+    */
+   public int damage(){
+       Random rand = new Random();
+       return rand.nextInt(strength) + 1;
+   }
+   
+   /**
+    * Is this creature still alive?
+    * @return boolean
+    */
+   public boolean isAlive()
+   {
+       return hp > 0;
+   }
     
 }
---
+++ Human.java
@@ -0,0 +1,33 @@
+
+/**
+ * Human, min strength 5-max strength 18, min hp 5-max hp 30
+ *
+ * @author (Erik Cooke)
+ * @version (2019.11.05)
+ */
+public class Human extends Creature
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Human
+     */
+    public Human()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
